2b5aa5bc6b376d7a01779616848bd2a1c7655bed
Alex Nozdriukhin <alex-nozzz@mail.ru>
2020-01-20 23:36:01 +0300

    Add plugin's name before error message

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ module.exports = class MinifyBundledPlugin {
           const optimizer = optimizers[ext];
 
           try {
-            if (!optimizer) throw new Error(`no optimizers for '${ext}'`);
+            if (!optimizer) throw new Error(`${pluginName}: no optimizers for '${ext}'`);
             const res = optimizer(
               compilation.assets[name].source().toString(),
               this.optimizerOpts[ext],
